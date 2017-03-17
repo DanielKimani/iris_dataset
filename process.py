d0693997d3b053d26a9b6d7d8f2548cba8d65b34
Nasr Maswood <nmaswood@uchicago.edu>
2017-03-17 15:40:45 -0400

    foo

--- process.py
+++ process.py
@@ -1,23 +1,20 @@
-from csv import reader
 import numpy as np
 import pandas as pd
-
-
 
 def create_flower_species_dict(iris_pd):
 
     uniq_items = iris_pd['species'].unique()
     num_items = len(uniq_items)
 
-    return dict(zip(uniq_items, num_items))
+    return dict(zip(uniq_items, range(num_items)))
 
 def process_data():
 
     iris_pd = pd.read_csv("iris.csv")
     flower_species_dict = create_flower_species_dict(iris_pd)
-    iris_pid = iris['species_as_num'].map(flower_species_dict).astype(int)
+    iris_pd['species_map'] = iris_pd['species'].map(flower_species_dict).astype(int)
 
 
-# lol take a break here to do rainbow push project
+    return iris_pd.drop(["species"], axis = 1)
 
-
+print( process_data().head())
